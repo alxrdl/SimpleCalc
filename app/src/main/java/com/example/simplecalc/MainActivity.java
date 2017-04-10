e03b99c74221205d1a1e62cebb31a6b1df9dc29f
Axel Haze <blureyhd@gmail.com>
2017-04-10 15:38:28 +0300

    Initial commit

---
+++ app/src/main/java/com/example/simplecalc/MainActivity.java
@@ -0,0 +1,123 @@
+package com.example.simplecalc;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Додамо дві константи
+    final  int MENU_RESET_ID = 1;
+    final  int MENU_QUIT_ID = 2;
+
+    EditText etNum1;
+    EditText etNum2;
+
+    Button btnAdd;
+    Button btnSub;
+    Button btnMult;
+    Button btnDiv;
+
+    TextView tvResult;
+    String oper = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //знаходимо елементи за ідентифікаторами
+        etNum1 = (EditText) findViewById(R.id.etNum1);
+        etNum2 = (EditText) findViewById(R.id.etNum2);
+
+        btnAdd = (Button) findViewById(R.id.btnAdd);
+        btnSub = (Button) findViewById(R.id.btnSub);
+        btnMult = (Button) findViewById(R.id.btnMult);
+        btnDiv = (Button) findViewById(R.id.btnDiv);
+
+        tvResult = (TextView) findViewById(R.id.tvResult);
+
+        //Пишемо обробник
+
+        btnAdd.setOnClickListener(this);
+        btnSub.setOnClickListener(this);
+        btnMult.setOnClickListener(this);
+        btnDiv.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+    //Обявляємо змінні
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+
+        //Перевіряємо поля на пустоту
+        if (TextUtils.isEmpty(etNum1.getText().toString())
+                || TextUtils.isEmpty(etNum2.getText().toString())){
+            return;
+        }
+
+        //читаємо EditText і заповнюємо змінні числами
+        num1 = Float.parseFloat(etNum1.getText().toString());
+        num2 = Float.parseFloat(etNum2.getText().toString());
+
+
+        //оприділяємо натиснуту кнопку і виконуємо відповідну операцію
+        //в oper пишем операцію, потім будемо викуористовувати в виводі
+        switch (v.getId()) {
+            case R.id.btnAdd:
+                oper = "+";
+                result = num1 + num2;
+                break;
+            case  R.id.btnSub:
+                oper = "-";
+                result = num1 - num2;
+                break;
+            case R.id.btnMult:
+                oper = "*";
+                result = num1 * num2;
+                break;
+            case R.id.btnDiv:
+                oper = "/";
+                result = num1 / num2;
+                break;
+            default:
+                break;
+        }
+        //Формуємо рядок виводу
+        tvResult.setText((num1 + " " + oper + " " + num2 + " = " + result));
+
+    }
+    //Створення меню
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, MENU_RESET_ID, 0 , "Reset");
+        menu.add(0, MENU_QUIT_ID, 0 ,"Quit");
+        return super.onCreateOptionsMenu(menu);
+    }
+    //обробка натискання на пункти меню
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case MENU_RESET_ID:
+                //Очищаємо поля
+                etNum1.setText("");
+                etNum2.setText("");
+                tvResult.setText("");
+                break;
+            //Вихід з додатку
+            case MENU_QUIT_ID:
+                finish();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
